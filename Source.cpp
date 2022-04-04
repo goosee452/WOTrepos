4a317abb96d7740168a7976b98e7912e65b0587f
Victor <ivanvikvik@gmail.com>
2022-04-04 14:05:54 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,48 +3,6 @@
 
 using namespace std;
 
-class Tank {
-
-public:
-
-	string model;
-	string country;
-	double mass;
-	string type;
-	double cost;
-
-
-	Tank() {
-
-		model = "unknown";
-		country = "unknown";
-		mass = 0;
-		type = "unknown";
-		cost = 0;
-
-	}
-
-	Tank(string init_model, string init_country, double init_mass, string init_type, double init_cost) {
-
-		model = init_model;
-		country = init_country;
-		mass = init_mass;
-		type = init_type;
-		cost = init_cost;
-
-	}
-
-	string info() {
-
-		return "model: " + model + "\n" + "country: " + country + "\n" + "mass: " + to_string(mass) + "\n" + "type: " + type + "\n" + "cost in dollars: " + to_string(cost);
-	}
-
-	~Tank() {
-
-	}
-};
-
-
 int main() {
 	Tank tank0;
 	cout << endl << tank0.info() << "\n\n";
